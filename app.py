e34f542294a0978266d2f56c4ae247ddb5271085
arturvt@gmail.com <174l@gtech>
2016-05-22 17:20:40 -0300

    Changes

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ from chat import app, socketio
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', debug=True)
-    socketio.run(app,  debug=True)
+    socketio.run(app, policy_server=False, transports='websocket, xhr-polling, xhr-multipart')
--- chat/runserver.py
+++ chat/runserver.py
@@ -1,4 +1,4 @@
 from chat import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, policy_server=False, transports='websocket, xhr-polling, xhr-multipart')
