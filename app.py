e4415f5b999bc0c52c2d2a1a761d19603d3cb40a
arturvt@gmail.com <174l@gtech>
2016-05-22 16:58:38 -0300

    Adding requirements

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ from chat import app, socketio
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', debug=True)
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', debug=True)
